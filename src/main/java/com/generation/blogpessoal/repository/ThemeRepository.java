65ef7cdf6b2ad94199afc96193ca11fdfd8566ac
taaixsp <taai.xsp@hotmail.com>
2022-05-03 09:38:44 -0300

    Repository note added

--- src/main/java/com/generation/blogpessoal/repository/ThemeRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/ThemeRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Theme;
-
+@Repository
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
 
 	public List<Theme> findAllByDescriptionContainingIgnoreCase(@Param ("description") String description);
